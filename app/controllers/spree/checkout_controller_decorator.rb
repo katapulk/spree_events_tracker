8e4dcdaaa5a3618672f8be3a9ec5cee31781afca
Joel David Hernandez Cruz <jdavidhc1710@gmail.com>
2021-01-07 16:58:01 -0500

    replace class_eval with Module.decorator

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,22 +1,25 @@
-Spree::CheckoutController.class_eval do
+module Spree::CheckoutControllerDecorator
+  def self.prepended(base)
+    base.include Spree::CheckoutEventTracker
 
-  include Spree::CheckoutEventTracker
-
-  after_action :track_order_state_change, only: :edit
-  after_action :track_order_completion, only: :update, if: :confirm?
+    base.after_action :track_order_state_change, only: :edit
+    base.after_action :track_order_completion, only: :update, if: :confirm?
+  end
 
   private
-    def confirm?
-      previous_state == 'confirm'
-    end
+  def confirm?
+    previous_state == 'confirm'
+  end
 
-    def track_order_completion
-      track_activity(activity: :complete_order, previous_state: previous_state, next_state: 'complete')
-    end
+  def track_order_completion
+    track_activity(activity: :complete_order, previous_state: previous_state, next_state: 'complete')
+  end
 
-    def track_order_state_change
-      unless previous_state == next_state
-        track_activity(activity: :change_order_state, previous_state: previous_state, next_state: next_state)
-      end
+  def track_order_state_change
+    unless previous_state == next_state
+      track_activity(activity: :change_order_state, previous_state: previous_state, next_state: next_state)
     end
+  end
 end
+
+::Spree::CheckoutController.prepend(Spree::CheckoutControllerDecorator)
